8e470756b727f0ef4e73e4822176c337edc30547
杜之 <dreaming_java@163.com>
2017-08-26 14:25:02 +0800

    Update HelloMain.java

--- src/main/java/com/hello/github/HelloMain.java
+++ src/main/java/com/hello/github/HelloMain.java
@@ -3,6 +3,6 @@ package com.hello.github;
 public class HelloMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello github!");
+		System.out.println("Hello github ~!");
 	}
 }
