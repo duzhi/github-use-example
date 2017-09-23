75d95e92b7221bf740cb56558218830ab852b0e0
duzhi <dreaming_java@163.com>
2017-09-23 18:12:25 +0800

    Revert "test"
    This reverts commit c49dd5715317b9891d18cd07989b57a135eefe0e.

--- src/main/java/com/hello/github/HelloMain.java
+++ src/main/java/com/hello/github/HelloMain.java
@@ -3,6 +3,6 @@ package com.hello.github;
 public class HelloMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello github ~2! ok");
+		System.out.println("Hello github ~2!");
 	}
 }
