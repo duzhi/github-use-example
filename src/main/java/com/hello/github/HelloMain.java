4348e54242a3932a5c268c691c75e74c30deff5b
duzhi <dreaming_java@163.com>
2017-11-09 01:06:24 +0800

    abc

--- src/main/java/com/hello/github/HelloMain.java
+++ src/main/java/com/hello/github/HelloMain.java
@@ -1,10 +1,16 @@
 package com.hello.github;
 
+import java.util.Arrays;
+
 public class HelloMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello github ~2! fix bug tag 0.0.2fix111");
 		System.out.println(Integer.MAX_VALUE);
-
+		String[] a = new String[]{"orderno_cp","orderno","order_amt"};
+		Arrays.sort(a);
+		for(String s:a) {
+		System.out.println(s);
+		}
 	}
 }
