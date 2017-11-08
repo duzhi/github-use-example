e7b70f9b054ad6cf97f62c22232eb103795cc209
duzhi <dreaming_java@163.com>
2017-11-09 01:07:35 +0800

    format

--- src/main/java/com/hello/github/HelloMain.java
+++ src/main/java/com/hello/github/HelloMain.java
@@ -7,10 +7,10 @@ public class HelloMain {
 	public static void main(String[] args) {
 		System.out.println("Hello github ~2! fix bug tag 0.0.2fix111");
 		System.out.println(Integer.MAX_VALUE);
-		String[] a = new String[]{"orderno_cp","orderno","order_amt"};
+		String[] a = new String[] { "orderno_cp", "orderno", "order_amt" };
 		Arrays.sort(a);
-		for(String s:a) {
-		System.out.println(s);
+		for (String s : a) {
+			System.out.println(s);
 		}
 	}
 }
